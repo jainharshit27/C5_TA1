0822a5dea4e00d762387bf9a1bbb00aae8f8573f
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-06-22 17:52:45 +0530

    Add files via upload

---
+++ C5 TA1.py
@@ -0,0 +1,31 @@
+#DATA TYPE
+a = "123"
+b = 123
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+a = 123
+print(type(a))
+b = 1.23
+print(type(b))
+c = True
+print(type(c))
+d = "abc"
+print(type(d))
+
+
+
+#TYPE CONVERSION
+a = "123"
+b = int(a)
+
+print(type(a))
+print(type(b))
+
+
+
+#CONCATENATION
+print("123" + "456")
+print(123 + 456)
